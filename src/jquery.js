a4bb026a0cb3fe2cc636e90828c5a6fae52031fe
jinchao1992 <jinchaophp@126.com>
2019-11-21 12:30:24 +0800

    添加find方法

--- src/jquery.js
+++ src/jquery.js
@@ -1,12 +1,37 @@
-window.jQuery = function (selector) {
-  const elements = document.querySelectorAll(selector)
-  const api = {
+window.jQuery = function (selectorOrArray) {
+  let elements
+  if (typeof selectorOrArray === 'string') {
+    elements = document.querySelectorAll(selectorOrArray)
+  } else if (selectorOrArray instanceof Array) {
+    elements = selectorOrArray
+  }
+  // const api = {...} return api 简写为下面的
+  return {
     addClass(className) {
+      className = className.trim()
+      const arr = className.split(' ')
       for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.add(className)
+        elements[i].classList.add(...arr)
       }
       return this
+    },
+    removeClass(className) {
+      className = className.trim()
+      const arr = className.split(' ')
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.remove(...arr)
+      }
+      return this
+    },
+    find(selector) {
+      let array = []
+      for (let i = 0; i < elements.length; i++) {
+        const elements2 = Array.from(elements[i].querySelectorAll(selector))
+        array = array.concat(elements2)
+      }
+      // console.log(this) // 此时的this依然是最外层函数调用返回的对象
+      // 这里的 return jQuery(array) 是创建新的作用域，防止跟之前API的相互污染
+      return jQuery(array)
     }
   }
-  return api
 }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
 const test = jQuery('.test')
-test.addClass('red').addClass('blue')
+test.addClass('red').addClass('blue') // 函数里面的this 就是获取的元素
+
+test.removeClass('red blue')
+
+const child = test.find('.child')
+
+child.addClass('blue')
+
+test.addClass('green')
