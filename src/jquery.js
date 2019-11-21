8e8738d843c266a73c56d01974903653afabf978
jinchao1992 <jinchaophp@126.com>
2019-11-21 15:19:22 +0800

    增加each方法等

--- src/jquery.js
+++ src/jquery.js
@@ -7,6 +7,7 @@ window.jQuery = function (selectorOrArray) {
   }
   // const api = {...} return api 简写为下面的
   return {
+    oldApi: selectorOrArray.oldApi,
     addClass(className) {
       className = className.trim()
       const arr = className.split(' ')
@@ -31,6 +32,36 @@ window.jQuery = function (selectorOrArray) {
       }
       // console.log(this) // 此时的this依然是最外层函数调用返回的对象
       // 这里的 return jQuery(array) 是创建新的作用域，防止跟之前API的相互污染
+      array.oldApi = this
+      return jQuery(array)
+    },
+    end() {
+      // 此时的 this 依然变成了新的api也就是操作find时返回的新创建的api
+      return this.oldApi
+    },
+    each(fn) {
+      for (let i = 0; i < elements.length; i++) {
+        fn.call(null, elements[i], i)
+      }
+      return this // 这里的 this 还是外层函数调用返回的对象
+    },
+    parent() {
+      const array = []
+      this.each((item) => {
+        if (array.indexOf(item.parentNode) === -1) {
+          array.push(item.parentNode)
+        }
+      });
+      return jQuery(array)
+    },
+    print() {
+      return elements
+    },
+    children() {
+      const array = []
+      this.each(item => {
+        array.push(...item.children)
+      })
       return jQuery(array)
     }
   }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 const test = jQuery('.test')
-test.addClass('red').addClass('blue') // 函数里面的this 就是获取的元素
+// test.addClass('red').addClass('blue') // 函数里面的this 就是获取的元素
 
-test.removeClass('red blue')
+// test.removeClass('red blue')
 
 const child = test.find('.child')
 
-child.addClass('blue')
+// child.addClass('blue').addClass('yellow')
 
-test.addClass('green')
+// test.each((item, i) => {
+//   console.log(item)
+//   console.log(`每一项test的下标为：${i}`)
+// })
+
+// const childParent = child.parent()
+// childParent.addClass('redme')
+
+// console.log(test.children().print())
+child.addClass('red')
